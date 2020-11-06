303ead24756497300c6408f275d776aa5d196d7a
ian-antking <ian.antking@gmail.com>
2020-11-06 08:49:15 +0000

    swap space and backspace keys

--- keyboards/40percentclub/gherkin/keymaps/ian-antking/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/ian-antking/keymap.c
@@ -1,9 +1,9 @@
 #include QMK_KEYBOARD_H
 
-#define FN1_SPC     LT(1, KC_SPC)
-#define FN2_BSPC    LT(2, KC_BSPC)
+#define FN1_BSPC    LT(1, KC_BSPC)
+#define FN2_SPC     LT(2, KC_SPC)
+#define FN3_TAB     LT(3, KC_TAB)
 #define FN3_DEL     LT(3, KC_DEL)
-#define FN3_SPC     LT(3, KC_SPC)
 #define LALT_C      LALT_T(KC_C)
 #define LGUI_V      LGUI_T(KC_V)
 #define RGUI_B      RGUI_T(KC_B)
@@ -18,19 +18,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ortho_3x10(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ESC,
-    SFT_Z,   CTL_X,   LALT_C,  LGUI_V,  FN2_BSPC,FN1_SPC, RGUI_B,  RALT_N,  CTL_M,   SFT_ENT
+    SFT_Z,   CTL_X,   LALT_C,  LGUI_V,  FN2_SPC,FN1_BSPC, RGUI_B,  RALT_N,  CTL_M,   SFT_ENT
   ),
 
   [1] = LAYOUT_ortho_3x10(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_TAB,  _______, _______, KC_GRV,  KC_QUOT, KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
-    _______, _______, _______, _______, FN3_DEL, _______, KC_SCLN, KC_COMM, KC_DOT,  KC_SLSH
+    _______, _______, _______, KC_GRV,  KC_QUOT, KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
+    _______, _______, _______, _______, FN3_TAB, _______, KC_SCLN, KC_COMM, KC_DOT,  KC_SLSH
   ),
 
   [2] = LAYOUT_ortho_3x10(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
     _______, _______, _______, _______, _______, KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,
-    _______, _______, _______, _______, _______, FN3_SPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    _______, _______, _______, _______, _______, FN3_DEL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   [3] = LAYOUT_ortho_3x10(
